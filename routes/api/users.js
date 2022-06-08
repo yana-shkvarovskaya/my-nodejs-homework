d75bd96cfe1a71aa21ad5a9036d9ec8006a8e3d8
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2022-06-08 19:37:23 +0300

    add avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { auth, validation, ctrlWrapper } = require("../../middlewares");
+const { auth, validation, ctrlWrapper, upload } = require("../../middlewares");
 const { users: ctrl } = require("../../controllers");
 const { joiAuthSchema, joiLoginSchema } = require("../../models/user");
 
@@ -12,5 +12,12 @@ router.post("/login", validation(joiLoginSchema), ctrlWrapper(ctrl.login));
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
+
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
 
 module.exports = router;
