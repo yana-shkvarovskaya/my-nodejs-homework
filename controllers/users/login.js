e3da0ff8696495c84a75b46bce2c2d6ca7607213
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2022-06-15 19:48:18 +0300

    hw06

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -10,8 +10,10 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
-  if (!user || !user.comparePassword(password)) {
-    throw new Unauthorized("Email or password is wrong");
+  if (!user || !user.verify || !user.comparePassword(password)) {
+    throw new Unauthorized(
+      "Email is wrong or not verified, or password is wrong"
+    );
   }
 
   const payload = {
